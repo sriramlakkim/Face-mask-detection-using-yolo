52345b6ad48d5421835fc38829c534e5b2568798
LAKKIMSETTY SRIRAMA KOTESWARA RAO <166588855+sriramlakkim@users.noreply.github.com>
2024-08-12 22:59:55 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,23 @@
+import cv2
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap.set(cv2.CAP_PROP_FPS, 30)
+
+if not cap.isOpened():
+    print("Error: Could not open webcam.")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to grab frame")
+        break
+
+    cv2.imshow("Webcam", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
